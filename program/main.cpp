151ef16f57d528616e9158d840b94511c88abea5
Isabela13 <imsp@dcc.ufmg.br>
2019-06-05 22:02:21 -0300

    alteracao gitignore

--- program/main.cpp
+++ program/main.cpp
@@ -8,33 +8,56 @@
 using namespace std;
 
 
+void administrativo(){
+    int value;
+    cout << "Ola o que voce deseja fazer?" << endl;
+    cout << "Digite 1 para matricular um aluno" << endl;
+    cout << "Digite 2 para criar uma turma" << endl;
+    cout << "Digite 3 para vincular um professor a uma disciplina" << endl;
+    cout << "Digite 4 para montar grade de horarios" << endl;
+    cin >> value;
+    if (value == 1){
+            string nome , dataNascimento , sexo , endereco , nomeResponsavelLegal;
+            int matricula , turma;
+            long telefone;
+            cout << "Digite o nome do aluno" << endl;
+            cin >> nome;
+            cout << "Digite a data de nascimento" << endl;
+            cin >> dataNascimento;
+            cout << "Digite o sexo do aluno" << endl;
+            cin >> sexo;
+            cout << "Digite o endereco" << endl;
+            cin >> endereco;
+            cout << "Digite o nome do responsavel legal pelo aluno" << endl;
+            cin >> nomeResponsavelLegal;
+            cout << "Digite o telefone do aluno" << endl;
+            cin >> telefone;
+            cout << "Digite a matricula do aluno" << endl;
+            cin >> matricula;
+            cout << "Digite o id da turma que o aluno sera vinculado" << endl;
+            cin >> turma;
+            Aluno c( nome,  dataNascimento,  sexo,  endereco,  telefone,  matricula,  nomeResponsavelLegal,  turma );
+            cout << "O aluno " << c.getNome() << " foi cadastrado com sucesso!";
+
+        }
+}
+
 int main(){
-    std::cout << "Use o main para testar seu programa!" << std::endl;
+    cout << "Entre com o seu perfil : (Aluno , Professor ou Administrativo)" << endl;
 
-    Aluno a(
-        "Isa", 
-        "14/10/1997", 
-        "Fem", 
-        "Rua a ", 
-        992739097, 
-        2016058000, 
-        "Edna", 
-        2014);
-    std::cout << a.getNome() <<" "<< a.getNascimento() <<" "<< a.getSexo() <<" "<< a.getEndereco() <<" "<< a.getTelefone() <<" " << a.getMatricula() <<" " << a.getNomeResponsavelLegal() <<" "<< a.getTurma() << std::endl;
-    a.setNome("Isabela Meneguci");
-    std::cout << a.getNome() << std::endl;
+    string perfil;
+    cin >> perfil;
 
+    if (perfil == "Aluno"){
+        cout << "Digite sua matricula";
 
-   Professor p ("Fabio", "25/12/1968", "Masc", "Rua b", 998739085, 5000.20, 40058327, "matematica" );
-    std::cout << p.getNome() <<" "<< p.getNascimento() <<" "<< p.getSexo() <<" "<< p.getEndereco() <<" "<< p.getTelefone()<<" "<<p.getSalario()<<" "<<p.getRegistro()<<" "<<p.getDisciplina() << std::endl;
-    p.setSalario(852.50);
-    std::cout << p.getSalario() << std::endl;
+    }else if (perfil == "Professor"){
 
-    FuncAdm f ("Carlos", "12/11/1967", "Masc", "Rua c", 998739785, 4000.10, 40058122, "diretoria" );
-    std::cout << f.getNome() <<" "<< f.getNascimento() <<" "<< f.getSexo() <<" "<< f.getEndereco() <<" "<< f.getTelefone()<<" "<<f.getSalario()<<" "<<f.getRegistro()<<" "<<f.getSetor() << std::endl;
-    f.setSetor("Sericos gerais");
-    f.setSalario(1500);
-    std::cout <<f.getSetor() <<" "<< f.getSalario() << std::endl;
+    }else if(perfil == "Administrativo"){
+        administrativo();
+    }else {
+        cout << "Perfil invalida";
+    }
 
     return 0;
     
